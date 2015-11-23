3543b41d9e25b5a8de1520f836858fda8540682e
Adam Dougal and Dominic Mullings <adam.dougal@sky.uk>
2015-11-23 14:32:37 +0000

    Exit the jvm when create injector fails

--- src/main/java/ru/vyarus/dropwizard/guice/injector/DefaultInjectorFactory.java
+++ src/main/java/ru/vyarus/dropwizard/guice/injector/DefaultInjectorFactory.java
@@ -4,6 +4,9 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Stage;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Default implementation of {@link ru.vyarus.dropwizard.guice.injector.InjectorFactory} that simply delegates
@@ -12,10 +15,22 @@ import com.google.inject.Stage;
  * @author Nicholas Pace
  * @since Dec 26, 2014
  */
+@SuppressFBWarnings("DM_EXIT")
 public class DefaultInjectorFactory implements InjectorFactory {
+
+    private final Logger logger = LoggerFactory.getLogger(DefaultInjectorFactory.class);
 
     @Override
     public Injector createInjector(final Stage stage, final Iterable<? extends Module> modules) {
-        return Guice.createInjector(stage, modules);
+        Injector injector = null;
+
+        try {
+            injector = Guice.createInjector(stage, modules);
+        } catch (Exception e) {
+            logger.error("Failed to create guice injector", e);
+            System.exit(1);
+        }
+
+        return injector;
     }
 }
