63561463b41d17ea5926b2cbdbf8bfdb4a908c84
Vyacheslav Rusakov <vyarus@gmail.com>
2016-08-18 03:47:36 +0700

    test async filter, servlet and resource in both contexts

---
+++ src/test/groovy/ru/vyarus/dropwizard/guice/web/async/AsyncFilterTest.groovy
@@ -0,0 +1,69 @@
+package ru.vyarus.dropwizard.guice.web.async
+
+import io.dropwizard.Application
+import io.dropwizard.Configuration
+import io.dropwizard.setup.Bootstrap
+import io.dropwizard.setup.Environment
+import ru.vyarus.dropwizard.guice.GuiceBundle
+import ru.vyarus.dropwizard.guice.module.installer.feature.web.AdminContext
+import ru.vyarus.dropwizard.guice.test.spock.UseDropwizardApp
+import spock.lang.Specification
+
+import javax.servlet.*
+import javax.servlet.annotation.WebFilter
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 18.08.2016
+ */
+@UseDropwizardApp(AsyncFilterApp)
+class AsyncFilterTest extends Specification {
+
+    def "Check async filter"() {
+
+        expect: "filter works"
+        new URL("http://localhost:8080/asyncf").getText() == 'done!'
+        new URL("http://localhost:8081/asyncf").getText() == 'done!'
+
+    }
+
+    static class AsyncFilterApp extends Application<Configuration> {
+        @Override
+        void initialize(Bootstrap<Configuration> bootstrap) {
+            bootstrap.addBundle(GuiceBundle.builder()
+                    .useWebInstallers()
+                    .extensions(AsyncFilter)
+                    .build())
+        }
+
+        @Override
+        void run(Configuration configuration, Environment environment) throws Exception {
+
+        }
+    }
+
+    @AdminContext(andMain = true)
+    @WebFilter(urlPatterns = "/asyncf", asyncSupported = true)
+    static class AsyncFilter implements Filter {
+
+        @Override
+        void init(FilterConfig filterConfig) throws ServletException {
+        }
+
+        @Override
+        void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            final String thread = Thread.currentThread().name
+            final AsyncContext context = request.startAsync()
+            context.start({
+                assert thread != Thread.currentThread().name
+                println "async servlet"
+                context.getResponse().writer.write("done!")
+                context.complete()
+            })
+        }
+
+        @Override
+        void destroy() {
+        }
+    }
+}
---
+++ src/test/groovy/ru/vyarus/dropwizard/guice/web/async/AsyncServletTest.groovy
@@ -0,0 +1,66 @@
+package ru.vyarus.dropwizard.guice.web.async
+
+import io.dropwizard.Application
+import io.dropwizard.Configuration
+import io.dropwizard.setup.Bootstrap
+import io.dropwizard.setup.Environment
+import ru.vyarus.dropwizard.guice.GuiceBundle
+import ru.vyarus.dropwizard.guice.module.installer.feature.web.AdminContext
+import ru.vyarus.dropwizard.guice.test.spock.UseDropwizardApp
+import spock.lang.Specification
+
+import javax.servlet.AsyncContext
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 18.08.2016
+ */
+@UseDropwizardApp(AsyncServletApp)
+class AsyncServletTest extends Specification {
+
+    def "Check async servlet"() {
+
+        expect: "servlet works"
+        new URL("http://localhost:8080/async").getText() == 'done!'
+        new URL("http://localhost:8081/async").getText() == 'done!'
+
+    }
+
+    static class AsyncServletApp extends Application<Configuration> {
+        @Override
+        void initialize(Bootstrap<Configuration> bootstrap) {
+            bootstrap.addBundle(GuiceBundle.builder()
+                    .useWebInstallers()
+                    .extensions(AsyncServlet)
+                    .build())
+        }
+
+        @Override
+        void run(Configuration configuration, Environment environment) throws Exception {
+
+        }
+    }
+
+    @AdminContext(andMain = true)
+    @WebServlet(urlPatterns = "/async", asyncSupported = true)
+    static class AsyncServlet extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            final String thread = Thread.currentThread().name
+            final AsyncContext context = req.startAsync()
+            context.start({
+                assert thread != Thread.currentThread().name
+                println "async servlet"
+                context.getResponse().writer.write("done!")
+                context.complete()
+            })
+        }
+    }
+}
