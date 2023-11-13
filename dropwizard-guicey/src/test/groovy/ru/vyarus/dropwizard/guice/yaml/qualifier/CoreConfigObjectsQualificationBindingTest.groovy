18c5d001a05ff8118392f34df3aedbc0d8546230
Vyacheslav Rusakov <vyarus@gmail.com>
2023-11-13 16:13:12 +0700

    test binding core dropwizard configuration objects by overriding getter with qualifier

---
+++ dropwizard-guicey/src/test/groovy/ru/vyarus/dropwizard/guice/yaml/qualifier/CoreConfigObjectsQualificationBindingTest.groovy
@@ -0,0 +1,98 @@
+package ru.vyarus.dropwizard.guice.yaml.qualifier
+
+import com.google.inject.Inject
+import com.google.inject.name.Named
+import io.dropwizard.core.Application
+import io.dropwizard.core.Configuration
+import io.dropwizard.core.setup.Bootstrap
+import io.dropwizard.core.setup.Environment
+import io.dropwizard.metrics.common.MetricsFactory
+import ru.vyarus.dropwizard.guice.GuiceBundle
+import ru.vyarus.dropwizard.guice.debug.report.yaml.BindingsConfig
+import ru.vyarus.dropwizard.guice.debug.report.yaml.ConfigBindingsRenderer
+import ru.vyarus.dropwizard.guice.module.yaml.ConfigurationTree
+import ru.vyarus.dropwizard.guice.test.jupiter.TestGuiceyApp
+import spock.lang.Specification
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 13.11.2023
+ */
+@TestGuiceyApp(App)
+class CoreConfigObjectsQualificationBindingTest extends Specification {
+
+    @Inject
+    @Named("metrics")
+    MetricsFactory metrics
+
+    @Inject
+    Config config
+
+    @Inject
+    ConfigurationTree tree
+
+    def 'Check qualification bindings'() {
+
+        expect: "qualified bindings recognized"
+        metrics == config.getMetricsFactory()
+
+        and: "report correct"
+        render(new BindingsConfig()
+                .showCustomConfigOnly()) == """
+
+    Configuration object bindings:
+        @Config Config
+
+
+    Unique sub configuration objects bindings:
+
+        Config.metrics
+            @Config MetricsFactory = MetricsFactory{frequency=1 minute, reporters=[], reportOnStop=false}
+
+
+    Qualified bindings:
+
+        Config:
+            @Named("metrics") MetricsFactory = MetricsFactory{frequency=1 minute, reporters=[], reportOnStop=false}
+
+
+    Configuration paths bindings:
+
+        Config:
+            @Config("metrics") MetricsFactory = MetricsFactory{frequency=1 minute, reporters=[], reportOnStop=false}
+            @Config("metrics.frequency") Duration = 1 minute
+            @Config("metrics.reportOnStop") Boolean = false
+            @Config("metrics.reporters") List<ReporterFactory> (with actual type ArrayList<ReporterFactory>) = []
+"""
+    }
+
+    String render(BindingsConfig config) {
+        new ConfigBindingsRenderer(tree).renderReport(config)
+                .replaceAll("\r", "")
+                .replaceAll(" +\n", "\n")
+                .replaceAll('@\\d+[^]C \n]+', '@1111111')
+    }
+
+
+    static class App extends Application<Config> {
+        @Override
+        void initialize(Bootstrap<Config> bootstrap) {
+            bootstrap.addBundle(GuiceBundle.builder()
+                    .printCustomConfigurationBindings()
+                    .build())
+        }
+
+        @Override
+        void run(Config config, Environment environment) throws Exception {
+        }
+    }
+
+    static class Config extends Configuration {
+
+        @Named("metrics")
+        @Override
+        MetricsFactory getMetricsFactory() {
+            return super.getMetricsFactory()
+        }
+    }
+}
