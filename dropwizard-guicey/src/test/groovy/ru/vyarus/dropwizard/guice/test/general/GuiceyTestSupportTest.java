59da0760ad092a98a7050de4318255b95957c74d
Vyacheslav Rusakov <vyarus@gmail.com>
2023-11-22 13:48:21 +0700

    add missing guice support object run test
    (cherry picked from commit 39c046da3d43357d4fff3232492a83055d19e06d)

--- dropwizard-guicey/src/test/groovy/ru/vyarus/dropwizard/guice/test/general/GuiceyTestSupportTest.java
+++ dropwizard-guicey/src/test/groovy/ru/vyarus/dropwizard/guice/test/general/GuiceyTestSupportTest.java
@@ -1,6 +1,10 @@
 package ru.vyarus.dropwizard.guice.test.general;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.Key;
+import io.dropwizard.configuration.FileConfigurationSourceProvider;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import org.apache.commons.text.StringSubstitutor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.vyarus.dropwizard.guice.support.AutoScanApplication;
@@ -24,5 +28,17 @@ public class GuiceyTestSupportTest {
 
         Assertions.assertThatThrownBy(() -> support.getBean(DummyService.class))
                 .hasMessage("Injector not available");
+    }
+
+    @Test
+    void testRunWithinStartedSupport() throws Exception {
+        GuiceyTestSupport<TestConfiguration> support = new GuiceyTestSupport<>(AutoScanApplication.class,
+                "src/test/resources/ru/vyarus/dropwizard/guice/config.yml",
+                new SubstitutingSourceProvider(new FileConfigurationSourceProvider(), new StringSubstitutor()));
+
+        support.run(injector -> {
+            Preconditions.checkNotNull(injector.getInstance(DummyService.class));
+            return null;
+        });
     }
 }
