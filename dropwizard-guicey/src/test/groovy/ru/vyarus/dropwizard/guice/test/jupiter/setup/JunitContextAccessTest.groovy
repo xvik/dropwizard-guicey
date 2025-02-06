29da1862f912081830175e6e9d88ff3209b9a6e2
Vyacheslav Rusakov <vyarus@gmail.com>
2025-02-06 15:30:44 +0700

    add getJunitContext() method for TestEnvironmentSetup (@EnableSetup) to be able to configure test application with full context access (a setup object could register hooks) (discussion #388)
    (cherry picked from commit 48245f3929f8c54aef75cdeeea40e5fe25ee9be8)

---
+++ dropwizard-guicey/src/test/groovy/ru/vyarus/dropwizard/guice/test/jupiter/setup/JunitContextAccessTest.groovy
@@ -0,0 +1,65 @@
+package ru.vyarus.dropwizard.guice.test.jupiter.setup
+
+import com.google.inject.AbstractModule
+import io.dropwizard.core.Application
+import io.dropwizard.core.Configuration
+import io.dropwizard.core.setup.Bootstrap
+import io.dropwizard.core.setup.Environment
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import ru.vyarus.dropwizard.guice.AbstractTest
+import ru.vyarus.dropwizard.guice.GuiceBundle
+import ru.vyarus.dropwizard.guice.hook.GuiceyConfigurationHook
+import ru.vyarus.dropwizard.guice.test.jupiter.TestGuiceyApp
+import ru.vyarus.dropwizard.guice.test.jupiter.env.EnableSetup
+import ru.vyarus.dropwizard.guice.test.jupiter.env.TestEnvironmentSetup
+import ru.vyarus.dropwizard.guice.test.jupiter.env.TestExtension
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 06.02.2025
+ */
+@TestGuiceyApp(App)
+class JunitContextAccessTest extends AbstractTest {
+
+    @EnableSetup
+    static TestEnvironmentSetup setup = new TestEnvironmentSetup() {
+        @Override
+        Object setup(TestExtension extension) {
+            extension.hooks(new GuiceyConfigurationHook() {
+                @Override
+                void configure(GuiceBundle.Builder builder) {
+                    builder.modules(new AbstractModule() {
+                        @Override
+                        protected void configure() {
+                            bind(ExtensionContext).toInstance(extension.getJunitContext())
+                        }
+                    })
+                }
+            })
+            return null
+        }
+    }
+
+    @Inject
+    ExtensionContext context
+
+    def "Check junit context accessible"() {
+
+        expect: "context provided"
+        context != null
+        context.getRequiredTestClass() == JunitContextAccessTest
+    }
+
+    static class App extends Application<Configuration> {
+        @Override
+        void initialize(Bootstrap<Configuration> bootstrap) {
+            bootstrap.addBundle(GuiceBundle.builder().build())
+        }
+
+        @Override
+        void run(Configuration configuration, Environment environment) throws Exception {
+        }
+    }
+}
