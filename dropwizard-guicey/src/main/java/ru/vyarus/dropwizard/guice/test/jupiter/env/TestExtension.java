48245f3929f8c54aef75cdeeea40e5fe25ee9be8
Vyacheslav Rusakov <vyarus@gmail.com>
2025-02-06 15:30:44 +0700

    add getJunitContext() method for TestEnvironmentSetup (@EnableSetup) to be able to configure test application with full context access (a setup object could register hooks) (discussion #388)

--- dropwizard-guicey/src/main/java/ru/vyarus/dropwizard/guice/test/jupiter/env/TestExtension.java
+++ dropwizard-guicey/src/main/java/ru/vyarus/dropwizard/guice/test/jupiter/env/TestExtension.java
@@ -1,5 +1,6 @@
 package ru.vyarus.dropwizard.guice.test.jupiter.env;
 
+import org.junit.jupiter.api.extension.ExtensionContext;
 import ru.vyarus.dropwizard.guice.test.jupiter.ext.conf.ExtensionConfig;
 import ru.vyarus.dropwizard.guice.test.jupiter.ext.conf.ExtensionBuilder;
 
@@ -11,7 +12,26 @@ import ru.vyarus.dropwizard.guice.test.jupiter.ext.conf.ExtensionBuilder;
  */
 public class TestExtension extends ExtensionBuilder<TestExtension, ExtensionConfig> {
 
-    public TestExtension(final ExtensionConfig cfg) {
+    private final ExtensionContext context;
+
+    public TestExtension(final ExtensionConfig cfg, final ExtensionContext context) {
         super(cfg);
+        this.context = context;
+    }
+
+    /**
+     * This might be class or method context ("before all" or "before each"), depending on how guicey extension would
+     * be registered: in case of registration with a non-static field "before all" not called.
+     * <p>
+     * Note that guicey provide static method for accessing objects, stored in context, like:
+     * {@link ru.vyarus.dropwizard.guice.test.jupiter.ext.GuiceyExtensionsSupport#lookupSupport(
+     * org.junit.jupiter.api.extension.ExtensionContext)} or
+     * {@link ru.vyarus.dropwizard.guice.test.jupiter.ext.GuiceyExtensionsSupport#lookupInjector(
+     * org.junit.jupiter.api.extension.ExtensionContext)}.
+     *
+     * @return test extension context
+     */
+    public ExtensionContext getJunitContext() {
+        return context;
     }
 }
