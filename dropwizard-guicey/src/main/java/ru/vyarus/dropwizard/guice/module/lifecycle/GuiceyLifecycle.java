97e46ba0f2d7333b9406ea122d8abdaf2801ee44
Vyacheslav Rusakov <vyarus@gmail.com>
2025-02-28 15:17:37 +0700

    add BeforeInit guicey event (the first point with available Bootstrap, before any initialization performed)
    (cherry picked from commit 4b73b856ed8f2a18810622ab66bf0620989077c0)

--- dropwizard-guicey/src/main/java/ru/vyarus/dropwizard/guice/module/lifecycle/GuiceyLifecycle.java
+++ dropwizard-guicey/src/main/java/ru/vyarus/dropwizard/guice/module/lifecycle/GuiceyLifecycle.java
@@ -25,6 +25,14 @@ public enum GuiceyLifecycle {
      */
     ConfigurationHooksProcessed(ConfigurationHooksProcessedEvent.class),
     /**
+     * Special meta event, called before all {@link ru.vyarus.dropwizard.guice.GuiceBundle} configuration phase logic.
+     * {@link io.dropwizard.core.setup.Bootstrap} object is available, but dropwizard bundles (registered through
+     * guicey) are not yet registered (note that {@link ru.vyarus.dropwizard.guice.GuiceBundle} is not yet added to
+     * bootstrap also because dropwizard calls bundle initialization before registering bundle (and so all dropwizard
+     * bundles, registered by guicey, will run before {@link ru.vyarus.dropwizard.guice.GuiceBundle} run).
+     */
+    BeforeInit(BeforeInitEvent.class),
+    /**
      * Called after dropwizard bundles initialization (for dropwizard bundles registered through guicey api).
      * Not called if no bundles were registered.
      */
