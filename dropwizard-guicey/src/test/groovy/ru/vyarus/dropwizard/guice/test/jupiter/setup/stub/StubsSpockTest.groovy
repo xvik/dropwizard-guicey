6bc71b3337fca5dd2f13de10fdefd9e81b42593c
Vyacheslav Rusakov <vyarus@gmail.com>
2025-03-26 17:51:57 +0700

    add spock2 tests for new field extensions; rename Tracker.find into Tracker.findTracks for groovy compatibility
    (cherry picked from commit 8780a9682172a140d3136c43242e81fc51aadb33)

---
+++ dropwizard-guicey/src/test/groovy/ru/vyarus/dropwizard/guice/test/jupiter/setup/stub/StubsSpockTest.groovy
@@ -0,0 +1,103 @@
+package ru.vyarus.dropwizard.guice.test.jupiter.setup.stub
+
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import ru.vyarus.dropwizard.guice.support.DefaultTestApp
+import ru.vyarus.dropwizard.guice.test.jupiter.TestGuiceyApp
+import ru.vyarus.dropwizard.guice.test.jupiter.ext.stub.StubBean
+import ru.vyarus.dropwizard.guice.test.jupiter.ext.stub.StubLifecycle
+import spock.lang.Specification
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 26.03.2025
+ */
+@TestGuiceyApp(value = DefaultTestApp, debug = true)
+class StubsSpockTest extends Specification {
+
+
+    @Inject
+    Service1 service1
+
+    @Inject
+    Service2 service2
+
+    @Inject
+    Service service
+
+    @StubBean(Service1)
+    Service1Stub stub
+
+    @StubBean(Service2)
+    static Service2Stub stub2
+
+    void setupSpec() {
+        assert stub2
+    }
+
+    void setup() {
+        assert stub
+        assert stub2
+    }
+
+    def "Check stubs"() {
+
+        expect:
+        service1 == stub
+        service2 == stub2
+        "moon" == service.get()
+        stub.beforeCalled
+        !stub.afterCalled
+    }
+
+    static class Service1Stub extends Service1 implements StubLifecycle {
+
+        static boolean created
+
+        public boolean beforeCalled
+        public boolean afterCalled
+
+        Service1Stub() {
+            assert !created
+            created = true
+        }
+
+        @Override
+        String get() {
+            return "moon"
+        }
+
+        @Override
+        void before() {
+            beforeCalled = true
+        }
+
+        @Override
+        void after() {
+            afterCalled = true
+        }
+    }
+
+    static class Service2Stub extends Service2 {}
+
+    static class Service1 {
+
+        String get() {
+            return "sun"
+        }
+    }
+
+    static class Service2 {
+    }
+
+    @Singleton
+    static class Service {
+
+        @Inject
+        Service1 service1
+
+        String get() {
+            return service1.get()
+        }
+    }
+}
