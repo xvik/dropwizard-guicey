854ff0cb59befaa2fd19a8de92bd971643e634d2
Vyacheslav Rusakov <vyarus@gmail.com>
2025-04-03 18:02:41 +0700

    fix tests for java 21

--- guicey-spa/src/test/groovy/ru/vyarus/guicey/spa/err/MappingClashTest.groovy
+++ guicey-spa/src/test/groovy/ru/vyarus/guicey/spa/err/MappingClashTest.groovy
@@ -4,13 +4,13 @@ import io.dropwizard.core.Application
 import io.dropwizard.core.Configuration
 import io.dropwizard.core.setup.Bootstrap
 import io.dropwizard.core.setup.Environment
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.extension.ExtendWith
 import ru.vyarus.dropwizard.guice.GuiceBundle
 import ru.vyarus.guicey.spa.SpaBundle
 import spock.lang.Specification
 import uk.org.webcompere.systemstubs.jupiter.SystemStub
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension
-import uk.org.webcompere.systemstubs.security.SystemExit
 import uk.org.webcompere.systemstubs.stream.SystemErr
 
 /**
@@ -21,19 +21,14 @@ import uk.org.webcompere.systemstubs.stream.SystemErr
 class MappingClashTest extends Specification {
 
     @SystemStub
-    SystemExit exit
-    @SystemStub
     SystemErr err
 
     def "Check uri paths clash"() {
 
         when: "starting app"
-        exit.execute(() -> {
-            new App().run(['server'] as String[])
-        });
+        Assertions.assertThrows(RuntimeException.class, () -> new App().run(['server'] as String[]))
 
         then: "error"
-        exit.exitCode == 1
         err.text.contains("Assets servlet app2 registration clash with already installed servlets on paths: /app/*")
     }
 
@@ -50,6 +45,11 @@ class MappingClashTest extends Specification {
 
         @Override
         void run(Configuration configuration, Environment environment) throws Exception {
+        }
+
+        @Override
+        protected void onFatalError(Throwable t) {
+            throw new RuntimeException(t)
         }
     }
 }
