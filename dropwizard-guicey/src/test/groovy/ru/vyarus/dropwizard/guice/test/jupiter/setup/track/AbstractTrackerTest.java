312695a9e1f4647d37214e634ff3079f89b436f9
Vyacheslav Rusakov <vyarus@gmail.com>
2025-04-03 15:16:43 +0700

    fix win test flickering

--- dropwizard-guicey/src/test/groovy/ru/vyarus/dropwizard/guice/test/jupiter/setup/track/AbstractTrackerTest.java
+++ dropwizard-guicey/src/test/groovy/ru/vyarus/dropwizard/guice/test/jupiter/setup/track/AbstractTrackerTest.java
@@ -13,7 +13,7 @@ public abstract class AbstractTrackerTest extends AbstractPlatformTest {
         return out
                 .replaceAll("@[\\da-z]{6,10}", "@11111111")
                 .replaceAll("\\[\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d+]", "[2025-22-22 11:11:11]")
-                .replaceAll("\\d+\\.\\d+ ms {4,}", "11.11 ms      ")
-                .replaceAll("\\d+\\.\\d+ ms", "11.11 ms");
+                .replaceAll("\\d+(\\.\\d+)? ms {4,}", "11.11 ms      ")
+                .replaceAll("\\d+(\\.\\d+)? ms", "11.11 ms");
     }
 }
